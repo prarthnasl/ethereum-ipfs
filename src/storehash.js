3a177d554bb5faf1c45d5a17bc352d92b3b4e59f
Prarthna Sabharwal <prarthna.sl@gmail.com>
2019-10-07 18:09:25 +0530

    Updating index file

---
+++ src/storehash.js
@@ -0,0 +1,36 @@
+import web3 from './web3';
+//access our local copy to contract deployed on rinkeby testnet
+//use your own contract address
+const address = '0x2c5f796db2588C56fB046C1aF182c2C623e535Ac';
+//use the ABI from your contract
+const abi = [
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getHash",
+		"outputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"name": "sendHash",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
+export default new web3.eth.Contract(abi, address);
